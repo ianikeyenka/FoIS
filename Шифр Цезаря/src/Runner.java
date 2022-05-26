b0e59d26f8927c143b7f2091b43609e902923830
Ilya Anikeyenka <71770143+ianikeyenka@users.noreply.github.com>
2022-05-27 00:39:37 +0300

    Add files via upload

--- Шифр Цезаря/src/Runner.java
+++ Шифр Цезаря/src/Runner.java
@@ -6,15 +6,15 @@ public class Runner {
         System.out.println("Введите текст!");
         String text = sc.nextLine();
         text = text.toUpperCase();
+        //Если требуется вывести и проверить строку без пробелов, то заменяем 8 строчку на 10 и убираем 20-23
+        //text = text.replaceAll("\\s","").toUpperCase();
         char[] arrayText = text.toCharArray();
         System.out.println("Шифрование - (1)\nРасшифрование - (2)");
         int procedures = sc.nextInt();
-
-        if (procedures != 1 && procedures != 2){
+        if (procedures != 1 && procedures != 2) {
             System.out.println("Неправильный ввод!");
             System.exit(0);
         }
-
         char[] newArray = new char[arrayText.length];
         for (int i = 0; i < arrayText.length; i++) {
             if (arrayText[i] == ' ') {
